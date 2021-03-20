1466f8d6eed52884a64ae82c67511f097f5882a2
Ashish kumar <61488210+ak1021lg15@users.noreply.github.com>
2021-03-20 13:15:09 +0530

    Add files via upload

---
+++ src/Config/FirebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyBQfX3Kgs9Md5pGh8NcBZ57vIB6_R1KfBA',
+  authDomain: 'mygitapp-d9f80.firebaseapp.com',
+  projectId: 'mygitapp-d9f80',
+  storageBucket: 'mygitapp-d9f80.appspot.com',
+  messagingSenderId: '1091427971333',
+  appId: '1:1091427971333:web:4c1f329c06f5d7162dc76c',
+  measurementId: 'G-BN06RK0EM1',
+};
---
+++ src/Context/UserContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+export const UserContext = createContext();
+
+// it can act kind of global state where we can store all of the things
